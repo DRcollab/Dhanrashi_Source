e24b40ac6aee4ae135279cada9e63706ae168bdc
Debashis Chakraborty <debashis.chakraborty25@gmail.com>
2022-01-06 23:26:49 +0530

    Update action_circle.dart
    Removed unused imports

--- lib/components/action_circle.dart
+++ lib/components/action_circle.dart
@@ -1,36 +1,32 @@
-import 'package:dhanrashi_mvp/components/utilities.dart';
 import 'package:flutter/material.dart';
 import 'package:dhanrashi_mvp/components/constants.dart';
 
 class ActionCircle extends StatelessWidget {
+  Color color;
+  int index = 0;
+  int selectedIndex = 0;
+  double radius = 40;
+  Function onPreseed;
+  String imageSource = '';
 
- Color color;
- int index= 0;
- int selectedIndex = 0;
- double radius = 40 ;
- Function onPreseed;
-String imageSource = '';
-
- ActionCircle({
-   this.color = Colors.white70,
-  required this.index,
-  this.selectedIndex = 0,
-  this.radius = 40.0,
-  required this.onPreseed,
-   this.imageSource = 'images/profile_image'
-  });
+  ActionCircle(
+      {this.color = Colors.white70,
+      required this.index,
+      this.selectedIndex = 0,
+      this.radius = 40.0,
+      required this.onPreseed,
+      this.imageSource = 'images/profile_image'});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap:(){
+      onTap: () {
         this.onPreseed();
       },
-
       child: CircleAvatar(
         backgroundColor: kPresentTheme.themeColor,
-        radius:  this.radius * DefaultValues.adaptForSmallDevice(context),
-        backgroundImage:AssetImage(imageSource),
+        radius: this.radius * DefaultValues.adaptForSmallDevice(context),
+        backgroundImage: AssetImage(imageSource),
       ),
     );
   }
